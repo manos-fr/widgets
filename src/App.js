da20d2c58c8c0a397fcda40d53be1083b0fb3d54
manos-fr <manoskakarakis@gmail.com>
2021-04-21 17:50:45 +0300

    intiial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  { title: "What is React", content: "Great!!" },
+  { title: "Why React", content: "React is the best" },
+  { title: "How to use?", content: "By creating components" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
 }
