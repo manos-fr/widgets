6aae1b85fb0fcc31802b68d9bfb1e5017825c72f
manos-fr <manoskakarakis@gmail.com>
2021-05-25 12:02:59 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from "react";
 import Accordion from "./components/Accordion";
 
 const items = [
-  { title: "What is React", content: "Great!!" },
-  { title: "Why React", content: "React is the best" },
-  { title: "How to use?", content: "By creating components" },
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
 function App() {
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,28 +1,56 @@
-import { render } from "@testing-library/react";
 import React, { useState } from "react";
 
-export default function Accordion({ items }) {
+const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (index) => {
     setActiveIndex(index);
   };
+
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
-}
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
+
+// ** not working for some reason
+
+// import React, { useState } from "react";
+
+// export default function Accordion({ items }) {
+//   const [activeIndex, setActiveIndex] = useState(null);
+//   const onTitleClick = (index) => {
+//     setActiveIndex(index);
+//   };
+//   const renderedItems = items.map((item, index) => {
+//     const active = index === activeIndex ? "active" : "";
+//     return (
+//       <React.Fragment key={item.title}>
+//         <div className={"title ${active}"} onClick={() => onTitleClick(index)}>
+//           <i className="dropdown icon"></i>
+//           {item.title}
+//         </div>
+//         <div className={"content ${active}"}>
+//           <p>{item.content}</p>
+//         </div>
+//       </React.Fragment>
+//     );
+//   });
+//   return <div className="ui styled accordion">{renderedItems}</div>;
+// }
+// ** not working for some reason
